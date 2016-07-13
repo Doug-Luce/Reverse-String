7ff92d89edafab0706c54a38a1a9f6c21c43e6d7
luced <luced@DESKTOP-5LLLL49>
2016-07-13 14:31:22 -0700

    Add project files.

---
+++ Reverse String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                String toReverse;
+                Console.WriteLine("Input a string to reverse: ");
+                toReverse = Console.ReadLine();
+                char[] letters = toReverse.ToCharArray();
+                Array.Reverse(letters);
+                Console.WriteLine(letters);
+
+            }
+        }
+
+    }
+}
